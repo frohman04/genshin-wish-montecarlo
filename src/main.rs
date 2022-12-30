c3c4abaa867b275d9ca1890fef67744410c05843
Chris Lieb <chris.lieb@gmail.com>
2022-12-29 19:14:30 -0500

    Run one sim at a time, based on cli arg

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod sim;
 mod weapon;
 
 use crate::character::CharacterBannerSimParams;
+use crate::sim::BannerSim;
 use crate::weapon::WeaponBannerSimParams;
+use clap::error::ErrorKind;
 use clap::{crate_name, crate_version, Arg, Command};
 
 fn main() {
@@ -25,6 +27,22 @@ fn main() {
                 .value_parser(clap::value_parser!(u64))
                 .help("The RNG seed to use (if unset, will be nondeterministic)"),
         )
+        .arg(
+            Arg::new("character")
+                .short('c')
+                .long("character")
+                .num_args(0)
+                .conflicts_with("weapon")
+                .help("Run simulations on the character banner"),
+        )
+        .arg(
+            Arg::new("weapon")
+                .short('w')
+                .long("weapon")
+                .num_args(0)
+                .conflicts_with("character")
+                .help("Run simulations on the weapon banner"),
+        )
         .get_matches();
 
     match matches.get_one::<u64>("seed") {
@@ -37,29 +55,23 @@ fn main() {
 
     let iterations = *matches.get_one::<usize>("ITERATIONS").unwrap();
 
-    let mut sim = CharacterBannerSimParams::new_sim();
-    for run_i in 0..iterations {
-        let mut wish_count: u8 = 1;
-        while !sim.wish() {
-            wish_count += 1;
-        }
-        println!(
-            "Run {}: Won limited 5* character on wish {}",
-            run_i + 1,
-            wish_count
-        );
-    }
+    let mut sim: BannerSim = if matches.get_flag("character") {
+        CharacterBannerSimParams::new_sim()
+    } else if matches.get_flag("weapon") {
+        WeaponBannerSimParams::new_sim()
+    } else {
+        clap::Error::raw(
+            ErrorKind::MissingRequiredArgument,
+            "Must specify either --character or --weapon",
+        )
+        .exit();
+    };
 
-    let mut sim = WeaponBannerSimParams::new_sim();
-    for run_i in 0..iterations {
+    for _run_i in 0..iterations {
         let mut wish_count: u8 = 1;
         while !sim.wish() {
             wish_count += 1;
         }
-        println!(
-            "Run {}: Won limited 5* weapon on wish {}",
-            run_i + 1,
-            wish_count
-        );
+        println!("{}", wish_count);
     }
 }
